63ce8d3f4b1f1c1ec64d56de21ab5e906afac93f
TanyaDanilovich <tan.danilovich@gmail.com>
2023-08-22 16:06:29 +0300

    migrate to react router dom v.6

---
+++ src/router.tsx
@@ -0,0 +1,18 @@
+import {createBrowserRouter,} from "react-router-dom";
+import App from './App';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
+
+
+// const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <App/>,
+//         errorElement: <ErrorPage/>,
+//         children: [
+//             {
+//                 path: "contacts/:contactId",
+//                 element: <Contact/>,
+//             },
+//         ],
+//     },
+// ]);
